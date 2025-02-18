f071cce64d8b600cf15e703f4e928c1d9380999c
Minh Đỗ <minhdo@Minhs-MacBook-Pro.local>
2025-02-18 22:59:14 +0700

    add them

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
 
 for x in range(100):
     print(x, end =" ")
-        
+            
--- test1.c
+++ test1.c
@@ -5,8 +5,8 @@ int a;
 int main()
 {
 
-    scanf("%d", &a);
-    printf("%d", 2*a);
+    scanf("%d", &a); //đây là dòng nhập a
+    printf("%d", 2*a); //Đây là dòng code in ra số gấp 2 lần
 
     return 0;
 }
